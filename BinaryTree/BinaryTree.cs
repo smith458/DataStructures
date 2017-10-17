f1f0c73cf4b2027f96a6fbb17dd49b961e0e8d98
Unknown <sbsmith@beckman.com>
2017-10-17 08:19:44 -0400

    Added Binary tree and Binary tree word sorter

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryTree<T> : IEnumerable<T> where T: IComparable<T>
+    {
+        private BinaryTreeNode<T> _head;
+        private int _count;
+
+        public void Add(T value)
+        {
+            if (_head == null)
+            {
+                _head = new BinaryTreeNode<T>(value);
+            }
+            else
+            {
+                AddTo(_head, value);
+            }
+
+            _count++;
+        }
+
+        private void AddTo(BinaryTreeNode<T> node, T value)
+        {
+            if (value.CompareTo(node.Value) < 0)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new BinaryTreeNode<T>(value);
+
+                }
+                else
+                {
+                    AddTo(node.Left, value);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddTo(node.Right, value);
+                }
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            BinaryTreeNode<T> parent;
+            return FindWithParent(value, out parent) != null;
+        }
+
+        private BinaryTreeNode<T> FindWithParent(T value, out BinaryTreeNode<T> parent)
+        {
+            BinaryTreeNode<T> current = _head;
+            parent = null;
+
+            while (current != null)
+            {
+                int result = value.CompareTo(current.Value);
+
+                if (result < 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }
+                else if (result > 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return current;
+        }
+
+        public bool Remove(T value)
+        {
+            BinaryTreeNode<T> current, parent;
+
+            current = FindWithParent(value, out parent);
+
+            if (current == null)
+            {
+                return false;
+            }
+
+            _count--;
+
+            //Case 1: If current has no right child, then the left child replaces current
+            if (current.Right == null)
+            {
+                if (parent == null)
+                {
+                    _head = current.Left;
+                }
+                else
+                {
+                    int result = parent.CompareTo(current.Value);
+                    if (result > 0)
+                    {
+                        parent.Left = current.Left;
+                    }
+                    else
+                    {
+                        parent.Right = current.Left;
+                    }
+                }
+            }
+            //Case 2: Current has a right without a left
+            else if (current.Right.Left == null)
+            {
+                current.Right.Left = current.Left;
+
+                if (parent == null)
+                {
+                    _head = current.Right;
+                }
+                else
+                {
+                    int result = parent.CompareTo(current.Value);
+                    if (result < 1)
+                    {
+                        parent.Left = current.Right;
+                    }
+                    else
+                    {
+                        parent.Right = current.Right;
+                    }
+                }
+            }
+            //Case 3: Current's right has a left child
+            else
+            {
+                BinaryTreeNode<T> leftMost = current.Right.Left;
+                BinaryTreeNode<T> leftMostParent = current.Right;
+
+                while (leftMost.Left != null)
+                {
+                    leftMostParent = leftMost;
+                    leftMost = leftMostParent.Left;
+                }
+
+                leftMostParent.Left = leftMost.Right;
+
+                leftMost.Left = current.Left;
+                leftMost.Right = current.Right;
+
+                if (parent == null)
+                {
+                    _head = leftMost;
+                }
+                else
+                {
+                    int result = parent.CompareTo(leftMost.Value);
+                    if (result < 1)
+                    {
+                        parent.Left = leftMost;
+                    }
+                    else
+                    {
+                        parent.Right = leftMost;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public void PreOrderTraversal(Action<T> action)
+        {
+            PreOrderTraversal(action, _head);
+        }
+
+        private void PreOrderTraversal(Action<T> action, BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
+                action(node.Value);
+                PreOrderTraversal(action, node.Left);
+                PreOrderTraversal(action, node.Right);
+            }
+        }
+
+        public void PostOrderTraversal(Action<T> action)
+        {
+            PostOrderTraversal(action, _head);
+        }
+
+        private void PostOrderTraversal(Action<T> action, BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
+                PostOrderTraversal(action, node.Left);
+                PostOrderTraversal(action, node.Right);
+                action(node.Value);
+            }
+        }
+
+        public void InOrderTraversal(Action<T> action)
+        {
+            InOrderTraversal(action, _head);
+        }
+
+        private void InOrderTraversal(Action<T> action, BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
+                InOrderTraversal(action, node.Left);
+                action(node.Value);
+                InOrderTraversal(action, node.Right);
+            }
+        }
+
+        public IEnumerator<T> InOrderTraversal()
+        {
+            if (_head != null)
+            {
+                Stack<BinaryTreeNode<T>> stack = new Stack<BinaryTreeNode<T>>();
+
+                BinaryTreeNode<T> current = _head;
+
+                bool goLeftNext = true;
+
+                stack.Push(current);
+
+                while (stack.Count > 0)
+                {
+                    if (goLeftNext)
+                    {
+                        while (current.Left != null)
+                        {
+                            stack.Push(current);
+                            current = current.Left;
+                        }
+                    }
+
+                    yield return current.Value;
+
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                        goLeftNext = true;
+                    }
+                    else
+                    {
+                        current = stack.Pop();
+                        goLeftNext = false;
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InOrderTraversal();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+    }
+}
---
+++ BinaryTreeWordSorter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+
+namespace BinaryTreeWordSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<string> tree = new BinaryTree<string>();
+
+            string input = string.Empty;
+
+            while (!input.Equals("quit", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.Write("> ");
+                input = Console.ReadLine();
+
+                string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string word in words)
+                {
+                    tree.Add(word);
+                }
+
+                Console.WriteLine("{0} words", tree.Count);
+
+                foreach (string word in tree)
+                {
+                    Console.Write("{0} ", word);
+                }
+
+                Console.WriteLine();
+                tree.Clear();
+            }
+
+        }
+    }
+}
