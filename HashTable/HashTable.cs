e0638ec65ffd105c6ad65f75ca6e0487384a23f9
Unknown <sbsmith@beckman.com>
2017-10-20 07:58:07 -0400

    Partially Added Hash Table

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class HashTable<TKey, TValue>
+    {
+        const double _fillFactor = .75;
+
+        int _maxItemsAtCurrentSize;
+
+        int _count;
+
+        HashTableArray<TKey, TValue> _array;
+
+        public HashTable() : this(1000)
+        {
+        }
+
+        public HashTable(int initialCapacity)
+        {
+            //watch at 3:00
+        }
+    }
+}
---
+++ HashTable/HashTableArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashTableArray<TKey, TValue>
+    {
+        HashTableArrayNode<TKey, TValue>[] _array;
+
+        public HashTableArray(int capacity)
+        {
+            _array = new HashTableArrayNode<TKey, TValue>[capacity];
+            for (int i = 0; i < capacity; ++i)
+            {
+                _array[i] = new HashTableArrayNode<TKey, TValue>();
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _array[GetIndex(key)].Add(key, value);
+        }
+
+        public void Update(TKey key, TValue value)
+        {
+            _array[GetIndex(key)].Update(key, value);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return _array[GetIndex(key)].Remove(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _array[GetIndex(key)].TryGetValue(key, out value)
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (HashTableArrayNode<TKey, TValue> node in _array)
+            {
+                node.Clear();
+            }
+        }
+    }
+}
